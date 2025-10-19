f072b0485be55e80c8c06f75461396b4355d8b32
DMaroe <dylanpribadi@gmail.com>
2025-10-19 20:18:36 +1100

    BUG: fix terraform

--- infra/main.tf
+++ infra/main.tf
@@ -9,6 +9,7 @@ terraform {
     bucket         = "s4125640-s4125656-bucket"
     key            = "terraform.tfstate" 
     region         = "us-east-1"
+    dynamodb_table = "terraform-lock"
     encrypt        = true
   }
 }
